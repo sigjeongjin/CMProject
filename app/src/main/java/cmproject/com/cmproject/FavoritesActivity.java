004b3072815ec4ecd21d11905f21cd8a58f89926
sigjeongjin <jadepoint@naver.com>
2017-06-23 11:32:03 +0900

    로그인추가

--- app/src/main/java/cmproject/com/cmproject/FavoritesActivity.java
+++ app/src/main/java/cmproject/com/cmproject/FavoritesActivity.java
@@ -9,5 +9,7 @@ public class FavoritesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorites);
+
+        String test = "민정이바보";
     }
 }
