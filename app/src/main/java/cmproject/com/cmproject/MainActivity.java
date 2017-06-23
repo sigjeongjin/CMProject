16180580e34428f482458ed3388147ca44f16796
sigjeongjin <jadepoint@naver.com>
2017-06-23 12:47:21 +0900

    로그인

--- app/src/main/java/cmproject/com/cmproject/MainActivity.java
+++ app/src/main/java/cmproject/com/cmproject/MainActivity.java
@@ -1,16 +1,33 @@
 package cmproject.com.cmproject;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnLogin, btnRegister;
+    EditText email, password;
+    private ProgressDialog pDialog;
+//    private SQLiteHandler db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       //fff
 
-        String testtest = "친절한 곰선생님aaaaaaa";
-    }
+        btnLogin = (Button)findViewById(R.id.btnLogin);
+        email = (EditText)findViewById(R.id.email);
+        password = (EditText)findViewById(R.id.password);
+        btnRegister = (Button)findViewById(R.id.btnregister);
+
+
+
+
+
+
+
+            }
 }
