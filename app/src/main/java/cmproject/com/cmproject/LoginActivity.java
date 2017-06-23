a2fcd4b85712858843e3d10fe7e4e5889a3ee494
sigjeongjin <jadepoint@naver.com>
2017-06-23 15:09:16 +0900

    로그인액티비티추가

---
+++ app/src/main/java/cmproject/com/cmproject/LoginActivity.java
@@ -0,0 +1,30 @@
+package cmproject.com.cmproject;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    CheckBox autoLogin;
+    Button btnLogin, btnRegister;
+    EditText email, password;
+    //  SQLiteDatabase db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnLogin = (Button)findViewById(R.id.btnLogin);
+        email = (EditText)findViewById(R.id.email);
+        password = (EditText)findViewById(R.id.password);
+        btnRegister = (Button)findViewById(R.id.btnregister);
+        autoLogin = (CheckBox)findViewById(R.id.checkbox);
+//        db =(SQLiteDatabase)findViewById(R.id.SQLiteDatabase);
+    }
+}
+
+
