2874d27a3e5a6d182134146bf3b130d2409b3044
sigjeongjin <jadepoint@naver.com>
2017-06-23 12:39:44 +0900

    mainactivity에 각 화면으로 갈 수 버튼 UI 추가 PUSH HISTORY 화면 추가

--- app/src/main/java/cmproject/com/cmproject/MainActivity.java
+++ app/src/main/java/cmproject/com/cmproject/MainActivity.java
@@ -1,14 +1,28 @@
 package cmproject.com.cmproject;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnPushHistoryActivityGo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       //fff
+
+        btnPushHistoryActivityGo = (Button) findViewById(R.id.PushHistoryActivityGo);
+
+        btnPushHistoryActivityGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), PushHistoryActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
