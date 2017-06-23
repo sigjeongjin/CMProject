dcd330b4f16bfc8a6783733711b99ab46021b965
IOT-13 <jadepoint@naver.com>
2017-06-23 11:28:30 +0900

    Initial commit

---
+++ app/src/main/java/cmproject/com/cmproject/WmMapActivity.java
@@ -0,0 +1,13 @@
+package cmproject.com.cmproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WmMapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wm_map);
+    }
+}
