92c642f8c2bd37a11a5d142bf2656f3ab26a14c4
MinJeong <jadepoint@naver.com>
2017-06-26 17:20:22 +0900

    map 마커 상세정보

--- app/src/main/java/cmproject/com/cmproject/WmMapActivity.java
+++ app/src/main/java/cmproject/com/cmproject/WmMapActivity.java
@@ -6,12 +6,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class WmMapActivity extends AppCompatActivity implements OnMapReadyCallback {
@@ -37,7 +39,8 @@ public class WmMapActivity extends AppCompatActivity implements OnMapReadyCallba
             }
         });
 
-        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().
+                       findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);  // 메인 쓰레드에서 호출되어야 메인스레드에서 콜백이 실행
     }
 
@@ -46,23 +49,38 @@ public class WmMapActivity extends AppCompatActivity implements OnMapReadyCallba
         Log.d(TAG, "GoogleMap is ready.");
         map = googleMap;
 
-        LatLng Sensor = new LatLng(37.573974, 127.023666);
+        // sensor 위치 변수값
+        final LatLng Sensor = new LatLng(37.573974, 127.023666);
 
         // marker 표시
         // market 의 위치, 타이틀, 짧은설명 추가 가능.
         MarkerOptions marker = new MarkerOptions();
-        marker.position(Sensor);
-        marker.title("박현진"); // maker title
-        marker.snippet("바보");
-        map.addMarker(marker); // maker click 시 보여줌
-        //map.addMarker(marker).showInfoWindow(); 화면에 보여줌
+        marker.position(Sensor); // sensor 위치 설정
+        marker.title("Sensor");   // maker title
+        //marker.snippet("수질");
+        map.addMarker(marker);   // maker click 시 보여줌
+        //map.addMarker(marker).showInfoWindow(); 화면에 바로 보여줌
 
         //movecamera 좌표를 설정한 곳으로 이동
+        //Google 지도는 zoom은 1~23까지 가능
         map.moveCamera(CameraUpdateFactory.newLatLng(Sensor));
         map.animateCamera(CameraUpdateFactory.zoomTo(10));
 
 //        CameraUpdate zoom = CameraUpdateFactory.zoomTo(15);
 //        googleMap.animateCamera(zoom);
+
+        googleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                // 마커 클릭시 호출되는 콜백 메서드
+                Toast.makeText(getApplicationContext(),
+                        marker.getTitle() + " 클릭했음"
+                        , Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(), WmInfoActivity.class);
+                startActivity(intent);
+                return false;
+            }
+        });
 
     }
 }
