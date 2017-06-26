4b04984576e03803f6ac0829a53f20bf39cae6f1
MinJeong <jadepoint@naver.com>
2017-06-26 10:28:35 +0900

    no message

--- app/src/main/java/cmproject/com/cmproject/FavoritesActivity.java
+++ app/src/main/java/cmproject/com/cmproject/FavoritesActivity.java
@@ -9,7 +9,7 @@ public class FavoritesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorites);
-
+        //fdghgkjdghk
 
     }
 }
